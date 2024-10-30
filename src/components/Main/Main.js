64dafb5da3b42a5d07b524447a63a7367a9997db
Ewelina <ewelinapuz@gmail.com>
2024-10-30 15:26:34 +0100

    add new text

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,21 +1,22 @@
 import React from "react";
 import './Main.css';
 import images from '../../assets/images/main.jpg';
-import star from '../../assets/images/star.svg';
+import foto1 from '../../assets/images/foto1.jpg';
 
 function Main() {
     return(
         <body>
+            <img src={images} alt='img' className='img' />
             <div>
-                <img src={images} alt='img' className='img' />
                 <p className='welcome'> <b>WITAJ W MAX DENT PRAKTYCE STOMATOLOGICZNEJ</b></p>
                 <p className='text-welcom'> Witaj w miejscu, gdzie zdrowy uśmiech spotyka się z nowoczesną opieką medyczną! Zadbamy o Twoje zęby w przyjaznej i bezstresowej atmosferze. Postaw na zdrowie i komfort!</p>
-                <img src={star} alt='star' className='star' />          
             </div>
-            <div>
-                <div className='aboutas'>Nasz gabinet stomatologiczny to miejsce, w którym pasja do zdrowego uśmiechu spotyka się z najnowocześniejszą technologią i troskliwą opieką nad pacjentem. Oferujemy szeroki zakres usług stomatologicznych, od profilaktyki i higieny jamy ustnej, przez leczenie zachowawcze, aż po zaawansowane zabiegi protetyczne, implantologiczne i ortodontyczne. Kładziemy duży nacisk na komfort pacjenta, dlatego każda wizyta odbywa się w przyjaznej bezstresowej atmosferze. Zdajemy sobie sprawę, że dla wielu osób wizyta u dentysty może być stresująca, dlatego dokładamy wszelkich starań, aby każdy czuł się u nas bezpiecznie i komfortowo. Nasze gabinety wyposażone są w nowoczesny sprzęt diagnostyczny i zabiegowy, co pozwala nam na precyzyjne planowane leczenia i jego skuteczne przeprowadzenia.
-            </div>
-            <img src={star} alt='star'className='star2' />
+            <img src={foto1} alt='img' className='foto1' />
+            <h1 className='First-visit'><b>PIERWSZA WIZYTA</b></h1>
+            <div className='visit-text'>
+                <div>Pierwsza wizyta to ważny krok w kierunku zdrowego uśmiechu, dlatego staramy się, aby każde pierwsze wrażenie było pozytywne i komfortowe</div>
+                <div>Indywidualne podejście do pacjenta jest dla nas priorytetem - każda osoba odwiedzająca nasz gabinet może liczyć na pełne zrozumienie swoich potrzeb oraz dokładne omówienie proponowanego planu leczenia. Stawiamy na otwartą komunikację i współpracę, aby wspólnie zadbać o zdrowie i estetykę Twojego uśmiechu. </div>
+                <div>Naszym celem jest nie tylko leczenie, ale również edukacja pacjentów o znaczeniu profilaktyki i zdrowych nawyków. Zależy nam na tym, abyś opuszczał nasz gabinet z poczuciem pewności, że jesteś w dobrych rękach. Zapraszamy do umówienia się na wizytę i dołączenia do grona naszych zdrowych pacjentów.</div>
             </div>
         </body>
         
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,8 +5,9 @@ import './Navbar.css'
 function Navbar() {
   return (
     <header>
-      <img src={logow} alt='logo' className='white-logo'/>
+      
       <nav>
+      <img src={logow} alt='logo' className='white-logo'/>
         <a href='link' ><b>NASZA OFERTA</b></a>
         <a href='link' ><b>UL. NOWA 13, OLKUSZ</b></a>
         <a href='link' className='number'><b>500 900 500</b></a>
