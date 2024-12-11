2dab20acbc8eef9aad581b3cf6a96eb15ad94c6a
Ewelina <ewelinapuz@gmail.com>
2024-12-11 14:37:10 +0100

    paths

---
+++ src/components/Offer/paths.ts
@@ -0,0 +1,11 @@
+export enum Paths {
+    IMPLANTOLOGIA = '/implantologia',
+    ENDODONCJA = '/endodoncja',
+    CHIRURGIA = '/chirurgia',
+    PROTETYKA = '/protetyka',
+    STOMATOLOGIAZACHOWAWCZA = '/stomatologia-zachowawcza',
+    ORTODONCJA = '/ortodoncja',
+    DIAGNOSTYKA = '/diagnostyka',
+    WYBIELANIEZĘBÓW = '/wybielanie-zebow',
+  }
+  
