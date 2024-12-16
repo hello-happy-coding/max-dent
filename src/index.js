5ac3aef16886da3721897691b0a41b3d3fe5dadf
Ewelina <ewelinapuz@gmail.com>
2024-12-16 23:54:36 +0100

    size

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import OfferDetail from "./components/OfferDetail/OfferDetail";
 import ImplantPhoto from "./assets/images/implanty.jpg";
-import './fonts/Lora.ttf';
+import './fonts/Libre.ttf';
 
 
 
