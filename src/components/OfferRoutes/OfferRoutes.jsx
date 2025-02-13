717f17f5c0164fbac846f0c5290dac06d1e31aff
Iga <pl.iga.zakrzewska@gmail.com>
2025-02-13 17:32:56 +0100

    remove unused files

--- src/components/OfferRoutes/OfferRoutes.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const OfferRoutes = () => {
-  return <>{/* react fragment - gdy musze mieć tego samego rodzica */}</>;
-};
-
-export default OfferRoutes;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import OfferDetail from "./components/OfferDetail/OfferDetail";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route
-          path="/implantologia"
-          element={
-            //  <OfferDetail id="implantologia" srcImg={ImplantPhoto} text="text" />
-            <OfferDetail id="implantologia" text="text" />
-          }
-        />
-        <Route path="/protetyka" element={<OfferDetail id="protetyka" />} />
-        <Route path="/chirurgia" element={<OfferDetail id="chirurgia" />} />
-        <Route path="/endodoncja" element={<OfferDetail id="endodoncja" />} />
-        <Route path="/diagnostyka" element={<OfferDetail id="diagnostyka" />} />
-        <Route
-          path="/stomatologia-zachowawcza"
-          element={<OfferDetail id="stomatologiazachowawcza" />}
-        />
-        <Route path="/ortodoncja" element={<OfferDetail id="ortodoncja" />} />
-        <Route
-          path="/wybielanie-zebow"
-          element={<OfferDetail id="wybielanie-zębów" />}
-        />
-        {/* <OfferRoutes /> */}
-      </Routes>
-    </React.StrictMode>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 reportWebVitals();
