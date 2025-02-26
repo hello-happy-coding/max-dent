881a1f3877023615a7a206efaf92cdc6289ae812
EwelinaPuz <144039570+EwelinaPuz@users.noreply.github.com>
2025-02-26 20:20:01 +0100

    Clearing (#11)
    * change jpg to webp

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './Hero.css';
+import React from "react";
+import "./Hero.css";
 
-import heroImage from '../../assets/images/main.jpg';
+import heroImage from "../../assets/images/main.webp";
 
 const Hero = () => {
   return (
-    <div className='main-image-container'>
-      <img src={heroImage} alt='Main background' className='main-image' />
-      <div className='clinic-info'>
-        <span className='clinic-desc'>Prywatna Praktyka Stomatologiczna</span>
+    <div className="main-image-container">
+      <img src={heroImage} alt="Main background" className="main-image" />
+      <div className="clinic-info">
+        <span className="clinic-desc">Prywatna Praktyka Stomatologiczna</span>
         <br />
-        <span className='clinic-name'>MAX DENT</span>
+        <span className="clinic-name">MAX DENT</span>
       </div>
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Hero from '../Hero/Hero';
-import FirstVisit from '../FirstVisit/FirstVisit';
-import OurServices from '../OurServices/OurServices';
-import Places from '../Places/Places';
-import ContactForm from '../ContactForm/ContactForm';
-import ParallaxImage from '../ParallaxImage/ParallaxImage';
-import parallaxImageOne from '../../assets/images/parallax/two.jpg';
-import parallaxImageTwo from '../../assets/images/parallax/three.jpg';
-import Navbar from '../Navbar/Navbar';
+import React from "react";
+import Hero from "../Hero/Hero";
+import FirstVisit from "../FirstVisit/FirstVisit";
+import OurServices from "../OurServices/OurServices";
+import Places from "../Places/Places";
+import ContactForm from "../ContactForm/ContactForm";
+import ParallaxImage from "../ParallaxImage/ParallaxImage";
+import parallaxImageOne from "../../assets/images/parallax/two.webp";
+import parallaxImageTwo from "../../assets/images/parallax/three.webp";
+import Navbar from "../Navbar/Navbar";
 
 const Main = () => {
   return (
