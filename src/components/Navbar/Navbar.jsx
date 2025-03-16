c85f4503502c92bd9ec3326e112b4be36bdedbd7
Iga <pl.iga.zakrzewska@gmail.com>
2025-03-16 19:26:10 +0100

    tablet fix 3

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import Logo from "../Logo/Logo";
+import Logo from '../Logo/Logo';
 
-import "./Navbar.css";
-import { GiHamburgerMenu } from "react-icons/gi";
+import './Navbar.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -13,44 +13,44 @@ const Navbar = () => {
   };
 
   return (
-    <header className="nav-overlay">
-      <nav className="nav">
-        <div className="nav-container">
-          <button aria-label="Menu" className="hamburger" onClick={toggleMenu}>
+    <header className='nav-overlay'>
+      <nav className='nav'>
+        <div className='nav-container'>
+          <button aria-label='Menu' className='hamburger' onClick={toggleMenu}>
             <GiHamburgerMenu />
           </button>
-          <div className="logo-container">
-            <Logo width="auto" height="80px" />
+          <div className='logo-container'>
+            <Logo width='80px' height='auto' />
           </div>
 
           {/* Menu standardowe (ukryte powyżej 580px) */}
-          <div className="nav-right">
-            <a href="#nasza-oferta">
+          <div className='nav-right'>
+            <a href='#nasza-oferta'>
               <b>NASZA OFERTA</b>
             </a>
             <a
-              href="https://maps.app.goo.gl/Xx2G8JeoEFtho7Sm8"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://maps.app.goo.gl/Xx2G8JeoEFtho7Sm8'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               <b>UL. NOWA 13, OLKUSZ</b>
             </a>
-            <a href="tel:+48669000700">
+            <a href='tel:+48669000700'>
               <b>+48 669 000 700</b>
             </a>
           </div>
-          <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
-            <a href="#nasza-oferta">
+          <div className={`mobile-menu ${menuOpen ? 'open' : ''}`}>
+            <a href='#nasza-oferta'>
               <b>NASZA OFERTA</b>
             </a>
             <a
-              href="https://maps.app.goo.gl/Xx2G8JeoEFtho7Sm8"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://maps.app.goo.gl/Xx2G8JeoEFtho7Sm8'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               <b>UL. NOWA 13, OLKUSZ</b>
             </a>
-            <a href="tel:+48669000700">
+            <a href='tel:+48669000700'>
               <b>+48 669 000 700</b>
             </a>
           </div>
